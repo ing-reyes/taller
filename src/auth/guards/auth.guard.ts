cf37e1cbf7b4d49a1f2a968a5ef18e496f8df311
ing-reyes <ing.sistemas.reyes3@gmail.com>
2025-01-10 17:52:53 -0400

    refactor: :recycle: The readability of the code for maintenance has been improved
    The readability of the code for maintenance has been improved. Especially applying SOLID principles, single responsibility. Adding a method to verify the token and a method to get a user

--- src/auth/guards/auth.guard.ts
+++ src/auth/guards/auth.guard.ts
@@ -9,6 +9,13 @@ import { Request } from 'express';
 import { OmitPassword } from './../../common/types/users/omit-password.user';
 import { UsersService } from '../../users/users.service';
 import { ManagerError } from './../../common/errors/manager.error';
+import { ApiOneResponse } from './../../common/interfaces/api-response.interface';
+import { UserEntity } from './../../users/entities/user.entity';
+
+const UNAUTHORIZED_ERROR = new ManagerError({
+  type: 'UNAUTHORIZED',
+  message: 'Unauthorized!',
+});
 
 @Injectable()
 export class AuthGuard implements CanActivate {
@@ -20,26 +27,14 @@ export class AuthGuard implements CanActivate {
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest<Request>();
     const token = this.extractTokenFromHeader(request);
+    if (!token) {
+      throw UNAUTHORIZED_ERROR;
+    }
+    
     try {
-      if (!token) {
-        throw new ManagerError({
-          type: 'UNAUTHORIZED',
-          message: 'Unauthorized!',
-        });
-      }
+      const payload = await this.verifyToken(token);
 
-      const payload = await this.jwtService.verifyAsync<OmitPassword>(token, {
-        secret: process.env.JWT_SECRET,
-      });
-      if (!payload.id) throw new UnauthorizedException('Invalid token!');
-
-      const user = await this.usersService.findOne(payload.id);
-      if (!user) {
-        throw new ManagerError({
-          type: 'UNAUTHORIZED',
-          message: 'Unauthorized!',
-        });
-      }
+      await this.getUser(payload.id);
 
       request['user'] = payload;
     } catch (error) {
@@ -47,6 +42,24 @@ export class AuthGuard implements CanActivate {
     }
 
     return true;
+  }
+
+  private async verifyToken(token: string): Promise<OmitPassword> {
+    const payload = await this.jwtService.verifyAsync<OmitPassword>(token, {
+      secret: process.env.JWT_SECRET,
+    });
+    if (!payload.id) {
+      throw new UnauthorizedException('Invalid token!');
+    }
+    return payload;
+  }
+
+  private async getUser(userId: string): Promise<ApiOneResponse<UserEntity>> {
+    const user = await this.usersService.findOne(userId);
+    if (!user) {
+      throw UNAUTHORIZED_ERROR;
+    }
+    return user;
   }
 
   private extractTokenFromHeader(request: Request): string | undefined {
